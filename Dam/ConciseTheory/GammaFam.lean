842f83584ea5a53d95f6be23b109cfccb7e7165a
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-01 08:35:20 -0800

    very nice

--- Dam/ConciseTheory/GammaFam.lean
+++ Dam/ConciseTheory/GammaFam.lean
@@ -309,7 +309,8 @@ def infer : Expr → Option Expr
 
 def t_k : Expr := ⟪₂ ((, ((:: (((K Data) (I Data)) Data)) ((:: read_α) ((:: ((>> fst) read)) ((:: read_y) ((:: ((>> fst) read)) nil)))))) ((, nil) nil)) ⟫
 
-#eval infer ⟪₂ (I :t_k K) Data (I Data) ⟫
+#eval Expr.display_infer <$> infer ⟪₂ (I :t_k K) Data (I Data) Data Data ⟫
+#eval Expr.display_infer <$> infer ⟪₂ I Data ((I :t_k K) Data (I Data) Data Data) ⟫
 #eval infer ⟪₂ (I :t_k K) ⟫
 
 #eval norm_context <$> infer ⟪₂ K ⟫

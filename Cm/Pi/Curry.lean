6f18d7e434f449b86f701c8a54cb1782929d75cd
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-11 10:55:41 -0800

    remove apply_partial\2

--- Cm/Pi/Curry.lean
+++ Cm/Pi/Curry.lean
@@ -33,22 +33,6 @@ def test_both_partial : Except Error Expr := do
 def apply_partial : Expr :=
   (:: both (:: (quote both) (:: both (:: (quote (quote apply)) (:: both (:: (quote both) (:: both (:: const (quote id)))))))))
 
-/-
-:: apply (:: f (:: x y)) = (:: (:: (:: apply_partial₂ f) x) y)
--/
-def apply_partial₂ : Expr :=
-  let x := (quote (:: both (:: (quote id) id))) -- drops f, then drops y later and just returns x
-  let y := (quote (quote id)) -- drops f and x
-  let xy := (:: x y) -- x and y arguments
-  let f := (:: both (:: const const)) -- drops x and y
-  let app_at_end := (quote (quote (quote apply))) -- drops f, x, and y
-
-  -- quotations are inert, and we skip over them. useful for "control flow"
-  let both_x := (quote both) -- dropped by f, left for x
-  let both_y := (quote (quote both)) -- dropped by x, left for y
-
-  (:: both (:: both_x (:: both (:: both_y (:: both (:: both_x (:: both (:: both_y (:: both (:: app_at_end (:: both (:: f (:: both xy)))))))))))))
-
 def test_apply_partial : Except Error Expr := do
   let my_f := symbol "f"
   let my_x := symbol "x"
@@ -63,27 +47,14 @@ def test_apply_partial : Except Error Expr := do
 -- :: "f" "x"
 #eval test_apply_partial
 
-def test_apply_partial₂ : Except Error Expr := do
-  let my_f := symbol "f"
-  let my_x := symbol "x"
-  let my_y := symbol "y"
-
-  dbg_trace do_step run (:: apply (:: apply_partial₂ my_f))
-  dbg_trace do_step run (:: apply (:: (:: apply (:: apply_partial₂ my_f)) my_x))
-  do_step run (:: apply (:: (:: apply (:: (:: apply (:: apply_partial₂ my_f)) my_x)) my_y))
-
-#eval test_apply_partial₂
-
 /-
-curry = prepend?
+curry = apply_partial?
 
-curry f = (:: then_cons f)
-
-curry f = both_partial (const (:: then_cons f)) then_cons
-
-curry 
+apply_partial only works for one argument, which is confusing.
+:: apply technically works for n-ary arguments.
+More like one single massive array argument.
 -/
-def curry : Expr :=
+--def curry : Expr :=
   
 
 

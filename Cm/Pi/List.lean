319195727b9f17551933c5edb28c289e337e9ce4
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-13 11:36:57 -0800

    assert list args test

--- Cm/Pi/List.lean
+++ Cm/Pi/List.lean
@@ -246,11 +246,8 @@ namespace test_list
 def test_list_reverse (l : Expr) : Except Error Expr :=
   try_step_n run 1000 (:: apply (:: list.reverse l))
 
-#eval try_step_n run 100 (:: apply (:: list.reverse.match_args' (:: (:: (symbol "rec_with") (:: (symbol "rec_with") (:: (:: const nil) (symbol "cons_handler")))) (:: (symbol "x") (:: (symbol "xs") nil)))))
-#eval try_step_n run 100 (:: apply (:: list.reverse.nil_handler' (:: (:: (symbol "rec_with") (:: (symbol "rec_with") (:: (:: const nil) (symbol "cons_handler")))) (:: (symbol "x") (:: (symbol "xs") nil)))))
-#eval try_step_n run 100 (:: apply (:: list.reverse.state'' (:: (:: (symbol "rec_with") (:: (symbol "rec_with") (:: (symbol "nil handler") (symbol "cons_handler")))) (:: (symbol "x") (:: (symbol "xs") nil)))))
-#eval try_step_n run 1000 (:: apply (:: list.reverse (:: (symbol "a") (:: (symbol "b") (:: (symbol "c") nil)))))
---#eval test_list_reverse (:: (symbol "a") (:: (symbol "b") (:: (symbol "c") nil)))
+#eval test_list_reverse (:: (symbol "a") (:: (symbol "b") (:: (symbol "c") nil)))
+  >>= (pure <| · == (:: (symbol "c") (:: (symbol "b") (:: (symbol "a") nil))))
 
 end test_list
 

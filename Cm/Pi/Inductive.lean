51a93e2471d20ae3386629953fd78d82eb4915f2
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-13 00:41:23 -0800

    rec_nat gives same output for zero

--- Cm/Pi/Inductive.lean
+++ Cm/Pi/Inductive.lean
@@ -36,13 +36,18 @@ but succ_case gets passed (:: num (:: rec_nat (:: zero_case succ_case)))
 
 (:: apply (:: apply (:: rec_nat (:: zero_case succ_case))) (:: succ zero)) =
   (:: apply (:: succ_case (:: (:: zero_case succ_case) zero)))
+
+No nil delimeter after succ_case.
 -/
 
-def rec_nat.zero_case :=
+def rec_nat.self :=
   :: π (:: id nil)
 
+def rec_nat.zero_case :=
+  :: π (:: nil (:: π (:: id nil)))
+
 def rec_nat.succ_case :=
-  :: π (:: nil id)
+  :: π (:: nil (:: π (:: nil id)))
 
 def rec_nat.quote_succ :=
   :: both (:: (quote const) rec_nat.succ_case)
@@ -50,16 +55,19 @@ def rec_nat.quote_succ :=
 def rec_nat.quote_zero :=
   :: both (:: (quote const) rec_nat.zero_case)
 
--- produces (:: const (:: zero_case succ_case))
+-- produces (:: const (:: rec_nat (:: zero_case succ_case)))
 def rec_nat.quote_match_args : Expr :=
-  :: both (:: (quote const) (:: both (:: rec_nat.zero_case rec_nat.succ_case)))
+  :: both (:: (quote const) (:: both (:: rec_nat.self (:: rec_nat.zero_case rec_nat.succ_case))))
 
 -- id is the number argument
 -- produces a quoted (:: both (quote succ_case) (:: (quote (:: zero_case succ_case)) id))
 -- assuming succ_case and zero_case are in scope
 def rec_nat.quote_fix : Expr :=
-  :: both (:: (quote both) (:: rec_nat.quote_succ (:: rec_nat.quote_match_args (quote id))))
+  :: both (:: (quote both) (:: both (:: rec_nat.quote_succ (:: (quote id) rec_nat.quote_match_args))))
 
+/-
+Assumes rec_nat is the first argument, zero_case 2nd, succ_case 3rd
+-/
 def rec_nat : Expr :=
   -- assuming zero_case and succ_case are in scope
   -- mk_id is like id above in match_nat
@@ -70,3 +78,25 @@ def rec_nat : Expr :=
   let inner_eq := :: both (:: (quote eq) (:: both (:: rec_nat.zero_case mk_rec_succ)))
   .cons both (:: inner_eq (quote zero))
 
+/-
+rec_nat tests:
+- just plug in names for the functions first, to see if it's doing the fixpoint right.
+-/
+
+def test_rec_nat_zero_works : Except Error Bool := do
+  let zero_result ← do_step run (:: apply (:: (:: apply (:: match_nat (:: id id))) zero))
+  let zero_rec ← do_step run (:: apply (:: (:: apply (:: rec_nat (:: rec_nat (:: id id)))) zero))
+
+  dbg_trace zero_result
+  dbg_trace zero_rec
+
+  pure <| zero_result == zero_rec
+
+#eval test_rec_nat_zero_works
+
+/-
+Just to print out the cases, prepend identifiers.
+-/
+/-def test_rec_nat_symb : Except Error Expr := do
+  let my_succ_case := symbol "my_succ_case"
+  let my_zero_case := symbol "my_zero_case"-/

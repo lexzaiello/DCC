70c33513022f0a2118635e95390033f1eed6a729
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-13 04:57:57 -0800

    getting there

--- Cm/Pi/List.lean
+++ Cm/Pi/List.lean
@@ -5,11 +5,22 @@ import Cm.Pi.Util
 open Expr
 
 /-
-(:: apply (:: list.rec_with (:: list.rec_with (:: nil_case xs_case))))
+(:: apply (:: apply (:: list.rec_with (:: list.rec_with (:: nil_case xs_case)))) list)
 
 xs_case should accept the head of the list as an argument.
 Note that this is a somewhat unsafe operation on lists that are not
 delimited with nil.
+
+xs_case receives the head and tail of the list.
+
+inputs to xs_case:
+(:: apply (:: apply list.rec_with (:: list.rec_with (:: nil_case xs_case))) (:: x xs))
+
+NOTE:
+- this is the exact same as the nat recursor, except zero is changed to nil
+- we don't need to inject xs, since it's already in that position.
+
+:: π whatever handles it for us.
 -/
 
 def list.rec_with.self :=
@@ -18,6 +29,9 @@ def list.rec_with.self :=
 def list.rec_with.nil_case :=
   :: π (:: nil (:: π (:: id nil)))
 
+/-
+These are in the same position as with Nat's recursor.
+-/
 def list.rec_with.xs_case :=
   :: π (:: nil (:: π (:: nil id)))
 
@@ -28,6 +42,9 @@ def list.rec_with.match_args : Expr :=
 def list.rec_with.quoted_xs_case :=
   (:: both (:: (quote const) list.rec_with.xs_case))
 
+/-
+This is the only thing that changes
+-/
 def list.rec_with.xs_num :=
   :: π (:: nil id)
 
@@ -52,7 +69,50 @@ def list.rec_with : Expr :=
   let inner_eq := :: both (:: (quote eq) (:: both (:: list.rec_with.nil_case list.rec_with.quote_fix_and_run)))
   .cons both (:: inner_eq (quote nil))
 
+/-
+(:: apply (:: nat.plus (:: m n)))
+Recurse on m, base case n.
+
+each level, cons on one of our own elements.
+-/
+def list.append : Expr :=
+  /-
+    For the xs case, we receive (:: (:: list.rec_with ... stuff) (:: x xs))
+    so we just need to cons onto the (:: x xs).
+    This may not be the right order but we'll see.
+    We just want to cons the x part, not the xs.
+    That gets handled later.
+
+    TODO: see if this receives the very top head element.
+    may need to change recursor.
+    not sure.
+  -/
+  -- let do_cons' := :: π (:: (
+  let x := :: π (:: nil (:: π (:: id nil)))
+  let xs := :: π (:: nil (:: π (:: nil id)))
+  let do_app := :: both (:: (quote apply) (:: π (:: (:: both (:: (quote apply) id)) (:: π (:: nil id)))))
+  let do_cons := :: both (:: x do_app)
+
+  let m := :: π (:: id nil)
+  let n := :: π (:: nil const)
+
+  let nil_case := n
+
+  -- generates recursor
+  let do_rec := (:: both
+    (:: (quote apply) (:: both
+      (:: (quote list.rec_with) (:: both
+        (:: (quote list.rec_with)
+          (:: both (:: nil_case (quote do_cons)))))))))
+
+  (:: both (:: (quote apply) (:: both (:: do_rec m))))
+
 namespace test_list
+
+def list_append (a b : Expr) : Except Error Expr := do
+  try_step_n run 100 (:: apply (:: list.append (:: a b)))
+
+#eval list_append (:: (symbol "a") (:: (symbol "b") nil)) (:: (symbol "a") (:: (symbol "b") nil))
 
 /-
 Continue running the recursor on a list until we get to nil,

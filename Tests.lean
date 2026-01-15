64db9af8195771542a7d59b924b46da2851368f2
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-14 19:37:42 -0800

    move tests out of tests dir

---
+++ Tests.lean
@@ -0,0 +1,25 @@
+import Cm.Pi.Ast
+import Cm.Pi.Eval
+import Cm.Pi.Error
+import Cm.Pi.Infer
+import Cm.Pi.List
+import LSpec
+
+open LSpec
+open Expr
+
+def test_nat_plus_nat (m n : ℕ) (max_steps : ℕ := 100) : Except Error (Option ℕ) := do
+  let m' := Nat'.of_nat m
+  let n' := Nat'.of_nat n
+  try_step_n run max_steps (:: apply (:: nat.plus (:: m' n')))
+    >>= (pure ∘ Nat'.to_nat)
+
+def assert_nat_plus (m n : ℕ) (expected : ℕ) (max_steps : ℕ := 100) : Bool :=
+  ((· == expected) <$> ·) <$> (test_nat_plus_nat m n max_steps)
+    |> ((·.getD false) <$> ·)
+    |> (·.toOption.getD false)
+
+def main := lspecIO $ .ofList [
+    ("Nat", [test "Nat.plus 1 100 is 101" (assert_nat_plus 1 100 101 100)
+      , test "Nat.plus 20 19 is 39" (assert_nat_plus 20 19 39 5000)]),
+  ]

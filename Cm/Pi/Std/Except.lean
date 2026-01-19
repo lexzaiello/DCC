ea3e50d2400a0ce810add4c0f451e0c497ff29bf
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-19 00:15:22 -0800

    orM

--- Cm/Pi/Std/Except.lean
+++ Cm/Pi/Std/Except.lean
@@ -184,6 +184,16 @@ def Except'.bothM : Expr :=
                 (:: both (:: (quote Except'.s_ok)
                   (:: π (:: Except'.unwrap Except'.unwrap))))))))))))))))))))
 
+def Except'.orM : Expr :=
+  (:: both (:: (quote apply) (:: both (::
+    (:: both (:: (quote apply)
+      (:: both (:: (quote Except'.match_with) (:: both (::
+        (quote id)
+        (:: π (:: (quote const) id))))))))
+     (:: π (:: id nil))))))
+
+#eval try_step_n' 1000 (:: apply (:: Except'.orM (:: (:: apply (:: Except'.err (symbol "a"))) (:: apply (:: Except'.ok (symbol "b"))))))
+
 example : try_step_n' 100 (:: apply (:: Except'.bothM (:: (:: apply (:: Except'.ok (symbol "a"))) (:: apply (:: Except'.ok (symbol "b")))))) = .ok (:: (symbol "ok") (:: (symbol "a") (symbol "b"))) := rfl
 
 example : try_step_n' 100 (:: apply (:: Except'.bothM (:: (:: apply (:: Except'.err (symbol "a"))) (:: apply (:: Except'.ok (symbol "b")))))) = (.ok <| :: (symbol "error") (symbol "a")) := rfl

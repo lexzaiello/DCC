5b715debf8269e363af829bbaf675907605fcd71
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-20 15:58:35 -0800

    infer \pi

--- Cm/Pi/Infer2.lean
+++ Cm/Pi/Infer2.lean
@@ -105,6 +105,24 @@ def infer_both : Expr :=
       (quote Except'.bothM)
       (:: both (:: infer_both.infer_f infer_both.infer_g))))))))))
 
+def infer_π.infer_f : Expr :=
+  (:: both (:: (quote apply) (:: both (::
+    (args.read 0 (:: π (:: id nil))) (:: both (::
+    (args.read 0 (:: π (:: id nil))) (:: both (::
+    (args.read 0 (:: π (:: nil (:: π (:: id nil))))) (:: π (:: nil (:: π (:: id nil))))))))))))
+
+def infer_π.infer_g : Expr :=
+  (:: both (:: (quote apply) (:: both (::
+    (args.read 0 (:: π (:: id nil))) (:: both (::
+    (args.read 0 (:: π (:: id nil))) (:: both (::
+    (args.read 0 (:: π (:: nil (:: π (:: nil id))))) (:: π (:: nil (:: π (:: nil id))))))))))))
+
+def infer_π : Expr :=
+  (:: apply (:: curry (:: apply (:: curry (:: both (:: (quote apply)
+    (:: both (::
+      (quote Except'.bothM)
+      (:: both (:: infer_π.infer_f infer_π.infer_g))))))))))
+
 set_option maxRecDepth 1000
 example : try_step_n' 100 (:: apply (:: (:: apply (:: (:: apply (:: (:: apply (:: curry (:: apply (:: curry infer_both.infer_f)))) (symbol "infer_global"))) (:: (symbol "f") (symbol "g")))) (symbol "x"))) = (.ok (:: apply (:: (symbol "infer_global") (:: (symbol "infer_global") (:: (symbol "f") (symbol "x")))))) := rfl
 
@@ -145,7 +163,10 @@ def infer : Expr :=
       (match_infer
         (assert_whole (quote both))
         (:: both (:: (quote apply) (:: both (:: (quote infer_both) infer.self))))
-        (infer_fn' infer.self)))
+        (match_infer
+          (assert_whole (quote π))
+          (:: both (:: (quote apply) (:: both (:: (quote infer_π) infer.self))))
+          (infer_fn' infer.self))))
 
 set_option maxRecDepth 5000
 example : try_step_n' 500 (:: apply (:: infer (:: infer (:: id nil)))) = (.ok (:: Except'.s_ok TData)) := rfl
@@ -153,3 +174,5 @@ example : try_step_n' 500 (:: apply (:: infer (:: infer (:: id nil)))) = (.ok (:
 example : try_step_n' 1000 (:: apply (:: (:: apply (:: (:: apply (:: infer (:: infer both))) (:: id id))) nil)) = (.ok (:: Except'.s_ok (:: TData TData))) := rfl
 
 example : try_step_n' 1000 (:: apply (:: infer (:: infer (:: (:: both (:: id id)) nil)))) = (.ok (:: (symbol "ok") (:: (symbol "Data") (symbol "Data")))) := rfl
+
+example : try_step_n' 1000 (:: apply (:: infer (:: infer (:: (:: π (:: id id)) (:: nil nil))))) = (.ok (:: (symbol "ok") (:: (symbol "Data") (symbol "Data")))) := rfl

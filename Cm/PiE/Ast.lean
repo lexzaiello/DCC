47025a183a25b4863607a343e6475a13d003cc38
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-23 13:04:24 -0800

    piE

---
+++ Cm/PiE/Ast.lean
@@ -0,0 +1,39 @@
+
+/-
+π does list projection and the type of elements in its list must be fixed,
+so π is only polymorphic.
+
+π α β     (f : α → β) (g : (List α) → (List β))             : List α → List β
+
+similarly, both is only polymorphic since it needs to form a new list as well.
+HOWEVER, it does not pattern match on the list, so f and g receive the same
+term, so it is actually dependent.
+Note, however, that it does not apply the terms to each other as in the SK combiantor
+calculus. The common pattern to do this is (:: both (:: (quote apply) id)) where quote = (:: const ·).
+
+both α (β : α → Type) (f : ∀ (x : α), β x) (g : ∀ (x : α), List (β x)) (x : α), (List (β x))
+
+-- TODO: apply, list.cons
+
+Note: since const here is dependent, it can achieve the above (:: both (:: (quote apply) id)) pattern,
+which completes the S pattern in SK.
+
+const α β (x : α) (y : β x) : α
+
+Pretty standard.
+id α : α → α
+
+Nil itself forms a list.
+nil α : List α
+
+f and g in eq receive different values, since eq is checking definitional equality.
+TODO: is this level of dependency allowed? Note that x and y must have the same type.
+Does it make sense to check definitional equality across types?
+Do we want this power? Does it add anything? If eq is just doing definitional equality,
+the terms should have the same type if they are syntactically equal anyway.
+
+eq α (β : α → Type) (f : ∀ (x : α), β x) (g : ∀ (x : α), β x) (x : α) (y : α) : β x
+-/
+inductive Expr where
+  | π    : Expr → Expr → Expr
+  | both : Expr → Expr

164404ba771c565898eaa3c0d007f216e22714ac
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-31 13:05:21 -0800

    forgor to commit VeryDependent files

---
+++ Cm/Questions/VeryDependent2.lean
@@ -0,0 +1,263 @@
+import Mathlib.Data.Nat.Notation
+import Cm.Questions.Ast
+
+open Expr
+
+/-
+Research question to answer:
+Can we do better with \leane{(((f : Σ T) (x : α)) : ((T ::[x, Σ T]) snd))}?
+
+Our new inference:
+(((f : Σ T) (x : α)) : ((T ::[x, Σ T]) snd))
+(x : ((T ::[x, Σ T]) fst))
+
+We still expect an output type of the form ::[t_in, t_out]
+
+Note that Σ T receives ::[x, Σ t], so x should have a known type,
+since it is our argument. but Σ T is of type Type.
+Also, ::[x, Σ t] (id Type) : Type
+
+Answer to research question:
+This is almost certainly optimal.
+-/
+
+/-
+nil β
+fst accepts a projector.
+
+α and β in list projections always refer to the x and xs elements,
+respectively
+
+::[(x : α), (xs : β)]
+
+the projector π is of type β → γ
+γ is set to be β by default,
+since our argument is id
+-/
+def fst (α β : Expr) (γ : Expr := β) (fn : Expr := ($ id, β)) : Expr :=
+  ($ const', ::[β, ($ const', Ty, β, γ)], α, fn)
+
+/-
+The version of snd in SigmaCorr is kind of unfaithful.
+We might just want to select the second element and reject the first.
+
+::[(a : α), (b : β)] snd' = b
+const' β α
+
+This version just fetches the second element.
+-/
+def snd (α β : Expr) : Expr :=
+  ($ const', β, α)
+
+/-
+::[f, g] list
+= list g f
+-/
+def comp (f g : Expr) : Expr :=
+  ::[f, g]
+
+def mk_both (α β γ f g : Expr) : Expr :=
+  ($ both, α , β, γ, f, g)
+
+/-
+Arrows:
+Σ (mk_arrow α β) (x : α) = ::[α, β]
+-/
+def mk_arrow (α β : Expr) : Expr :=
+  ($ Σ
+  , ($ const', Ty, α, ::[α, β]))
+
+/-
+Point-free version of mk_arrow.
+
+(::[β, α] mk_arrow) x = ::[α, β]
+
+(::[β, α] mk_arrow) = (const' Ty α ::[β, α])
+
+::[β, α] : Ty
+
+(::[β, α] (const' Ty ) x
+= nil 
+-/
+def mk_arrow' : Expr :=
+  (nil 
+  sorry
+
+/-
+::[a, b] (snd' α β fn_post)
+= b fn_post
+
+(a : β) (b : α)
+-/
+def snd' (α β : Expr) (γ : Expr := α) (fn_post : Expr := ($ id, α)) :=
+  comp fn_post ($ const', (mk_arrow α γ), β, fn_post)
+
+/-
+id : (Σ Tid)
+
+Tid receives ::[α, id]
+we can insert a Ty ::[α, Ty, id]
+
+we want to make ::[Ty, Σ Tid₂]
+
+we can actually map the components to make this work.
+::[Tid₂, Ty] id = Tid₂
+Tid₂ already has sigma prepended.
+
+::[α, id] (π, (nil, Ty), ::[Tid₂, Ty])
+
+t_app_α α := ::[nil α, nil α] -- type of id α. this is the same as Id₂, but with α applied?
+t_both α := 
+
+
+Tid = (mk_both ? ? ? (
+-/
+
+/-
+id : (Σ Tid)
+::[α, id] Tid = ::[nil Ty α, Tid₂]
+
+for Tid₂, something similar
+Tid₂ ::[x, α, id] = ::[nil α x, nil α x]
+Tid₂ = 
+
+t_app_α α := ::[nil α, nil α]
+t_app_α 
+
+Tid = Σ (fst ? ? (mk_both (α := Ty) (β := Ty) (γ := Ty) (f := ($ nil, Ty)) (g := ($ const', Ty, Ty, Tid₂))))
+Tid₂ = Σ (
+-/
+abbrev id.type : Expr :=
+  -- Tid₂ ::[x, α, id]
+  -- this uses dependent const version of snd
+  -- snd' β(::[α, id]) =
+  -- ::[α, id]
+  let Tid₂ : Expr := ($ Σ, (snd' sorry sorry Ty (mk_both sorry sorry  sorry nil nil)))
+  ($Σ,
+    (fst
+      Tid₂
+      ($ nil, Ty)
+      (mk_both (α := Ty) (β := Ty) (γ := Ty)
+        (f := ($ nil, Ty))
+        (g := ($ nil, Tid₂)))))
+
+inductive IsStepStar {rel : Expr → Expr → Prop} : Expr → Expr → Prop
+  | refl  : IsStepStar e e
+  | trans : rel e₁ e₂
+    → IsStepStar e₂ e₃
+    → IsStepStar e₁ e₃
+
+inductive IsBetaEq {s : Expr → Expr → Prop} : Expr → Expr → Prop where
+  | rel   : s e₁ e₂ → IsBetaEq e₁ e₂
+  | refl  : IsBetaEq e e
+  | symm  : IsBetaEq e₁ e₂ → IsBetaEq e₂ e₁
+  | trans : IsBetaEq e₁ e₂ → IsBetaEq e₂ e₃ → IsBetaEq e₁ e₃
+
+inductive IsStep : Expr → Expr → Prop
+  | sapp   : IsStep ($ ::[x, f], fn) ($ fn, f, x)
+  | sigma  : IsStep ($ Σ, Γ, x) ($ Γ, x)
+  | nil    : IsStep ($ nil, α, x) α
+  | id     : IsStep ($ Expr.id, _α, x) x
+  | both   : IsStep ($ both, _α, _β, _γ, f, g, x)
+    ::[($ f, x), ($ g, x)]
+  | const' : IsStep ($ const', _α, _β, x, y) x
+  | left   : IsStep f f'
+    → IsStep ($ f, x) ($ f', x)
+  | right  : IsStep x x'
+    → IsStep ($ f, x) ($ f, x')
+
+inductive DefEq : Expr → Expr → Prop
+  | refl    : DefEq a a
+  | step    : IsStep e e' → DefEq e e'
+  | symm    : DefEq e₁ e₂ → DefEq e₂ e₁
+  | trans   : DefEq e₁ e₂ → DefEq e₂ e₃ → DefEq e₁ e₃
+  | left    : DefEq f f'  → DefEq ($ f, x) ($ f', x)
+  | right   : DefEq x x'  → DefEq ($ f, x) ($ f, x')
+  | lright  : DefEq f f'  → DefEq ::[x, f] ::[x, f']
+  | lleft   : DefEq x x'  → DefEq ::[x, f] ::[x', f]
+
+inductive IsStepN : ℕ → Expr → Expr → Prop
+  | one  : IsStep e e' → IsStepN 1 e e'
+  | succ : IsStep e e'' → IsStepN n e'' e'''
+    → IsStepN n.succ e e'''
+
+inductive ValidJudgment : Expr → Expr → Prop
+  /- TODO: Remove this in the actual calculus
+     use type universes
+     this module is just for answering reseach questions -/
+  | ty        : ValidJudgment Ty Ty
+  | sigma     : ValidJudgment ($ Σ, Γ) Ty
+  --| id        : ValidJudgment id Σ[::[nil, id, id], Ty]
+  /-
+    To check an app:
+    - functions have type Σ T
+    - (((f : Σ T) (x : α)) : ((T ::[x, Σ T]) snd))
+    - Body of f's type Σ T receives ::[x, Σ T] via list application:
+      ($ ::[x, Σ T] T)
+    - Fixpoint is useful to recover expected types later
+    - Body of f's type produces ::[t_in, t_out]
+    - Type of (f x) is ::[t_in, t_out] snd
+  -/
+  | app       : ValidJudgment f ($ Σ, T)
+    → ValidJudgment x ($ ($ ::[x, f], T), (fst Ty Ty))
+    → ValidJudgment ($ f, x) ($ ($ ::[x, f], T), (snd Ty Ty))
+  | def_eq    : ValidJudgment e α
+    → DefEq α β
+    → ValidJudgment e β
+
+/-
+More universal application rule:
+
+Question:
+Γ, ::[x, f] or ::[x, f] Γ?
+
+f : Σ T
+f x : (snd (T ::[x, f]))
+check x: (fst (T ::[x, f]))
+-/
+
+/-
+::[x, α, id]
+
+both ? ? ? (
+
+out_α_ty = ($ Σ, (both (
+
+assert_α_ty = (fst Ty Ty ($ nil, Ty))
+with_α_scope f = Σ ($ both, ($ nil, Ty), ($ nil Ty), assert_α_ty, 
+id : Σ ($ both, (nil Ty), (nil Ty), assert_α_ty, (const' ? Ty (
+-/
+
+theorem id_α_well_typed : ValidJudgment α Ty → ValidJudgment ($ id, α) Σ[::[id, id], ::[::[α, Ty], Ty]] := by
+  intro h_t
+  apply ValidJudgment.def_eq
+  apply ValidJudgment.app
+  apply ValidJudgment.id
+  apply ValidJudgment.def_eq h_t
+  apply DefEq.symm
+  apply DefEq.trans
+  apply DefEq.step
+  apply IsStep.sapp
+  apply DefEq.step
+  apply IsStep.nil
+  apply DefEq.trans
+  apply DefEq.sigma_t
+  apply DefEq.lleft
+  apply DefEq.lright
+  apply DefEq.trans
+  apply DefEq.step
+  apply IsStep.sapp
+  apply DefEq.step
+  apply IsStep.nil
+  apply DefEq.refl
+
+example : ValidJudgment α Ty → ValidJudgment x α → ValidJudgment ($ id, α, x) α := by
+  intro h_t h_t_x
+  apply ValidJudgment.def_eq
+  apply ValidJudgment.app
+  exact (@id_α_well_typed α h_t)
+  apply ValidJudgment.def_eq
+  exact h_t_x
+  
+  sorry
+

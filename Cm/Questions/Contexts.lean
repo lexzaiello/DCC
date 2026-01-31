ad931b26b0f576bf8f2eacfc56b2390c57338537
Alexandra Aiello <dowlandaiello@gmail.com>
2026-01-31 03:53:50 -0800

    id \alpha type-checks

--- Cm/Questions/Contexts.lean
+++ Cm/Questions/Contexts.lean
@@ -78,6 +78,8 @@ Ok, what about const's type?
 
 our sigma rule feels too restrictive.
 We need a nil case, as well for the asserts.
+
+Δ register should start out as nil.
 -/
 
 inductive IsStepStar {rel : Expr → Expr → Prop} : Expr → Expr → Prop
@@ -102,37 +104,87 @@ inductive IsStep : Expr → Expr → Prop
   | right  : IsStep x x'
     → IsStep ($ f, x) ($ f, x')
 
+inductive DefEq : Expr → Expr → Prop
+  | refl    : DefEq a a
+  | step    : IsStep e e' → DefEq e e'
+  | symm    : DefEq e₁ e₂ → DefEq e₂ e₁
+  | trans   : DefEq e₁ e₂ → DefEq e₂ e₃ → DefEq e₁ e₃
+  | left    : DefEq f f'  → DefEq ($ f, x) ($ f', x)
+  | right   : DefEq x x'  → DefEq ($ f, x) ($ f, x')
+  | lright  : DefEq f f'  → DefEq ::[x, f] ::[x, f']
+  | lleft   : DefEq x x'  → DefEq ::[x, f] ::[x', f]
+  | sigma_t : DefEq Δ Δ'  → DefEq Σ[Γ, Δ] Σ[Γ, Δ']
+
 inductive IsStepN : ℕ → Expr → Expr → Prop
   | one  : IsStep e e' → IsStepN 1 e e'
   | succ : IsStep e e'' → IsStepN n e'' e'''
     → IsStepN n.succ e e'''
 
-inductive valid_judgment : Expr → Expr → Prop
+inductive ValidJudgment : Expr → Expr → Prop
   /- TODO: Remove this in the actual calculus
      this module is just for answering reseach questions -/
-  | ty    : valid_judgment Ty Ty
-  | sigma : valid_judgment Σ[Γ, Δ] Ty
-  | id    : valid_judgment id Σ[::[id, nil, id, nil], ::[Ty, nil]]
-  | app'  : valid_judgment f Σ[::[t_out, nil], ::[α, Δs]]
-    → valid_judgment x α
-    → valid_judgment ($ f, x) t_out
-  | app   : valid_judgment f Σ[::[t_in, ::[x, xs]], ::[α, Δs]]
-    → valid_judgment x α
-    → IsStep ($ ::[arg, Δ], t_in) t'
-    → valid_judgment ($ f, x) Σ[::[x, xs], ::[::[t', Δ], nil]]
+  | ty        : ValidJudgment Ty Ty
+  | sigma     : ValidJudgment Σ[Γ, Δ] Ty
+  | id        : ValidJudgment id Σ[::[nil, id, id], Ty]
+  --| id        : ValidJudgment id Σ[::[id, nil, id, nil], ::[Ty, nil]]
+  /-| app_end   : ValidJudgment f Σ[::[t_out, nil], ::[::[α, β], Δs]]
+    → ValidJudgment x α
+    → ValidJudgment ($ f, x) t_out
+  | app_mid   : ValidJudgment f Σ[::[t_in, ::[x, xs]], ::[::[α, β], nil]]
+    → ValidJudgment x α
+    → ValidJudgment ($ f, x) Σ[::[x, xs], ::[::[($ ::[arg, ::[α, Δs]], t_in), ::[α, Δs]], nil]]
+  | app_start : ValidJudgment f Σ[::[α, ::[x, xs]], nil]
+    → ValidJudgment x α
+    → ValidJudgment ($ f, x) Σ[::[($ -/
+  -- Δ register receives the argument
+  -- head of Γ register receives ::[x, Δ]
+  -- to dictate the type of the argument
+  | app       : ValidJudgment f Σ[::[t_in, Γ], Δ]
+    → ValidJudgment x ($ ::[x, Δ], t_in)
+    → ValidJudgment ($ f, x) Σ[Γ, ::[::[x, ($ ::[x, Δ], t_in)], Δ]]
+  | singleton : ValidJudgment f Σ[t_out, Δ]
+    → ValidJudgment f ($ Δ, t_out)
+  | def_eq    : ValidJudgment e α
+    → DefEq α β
+    → ValidJudgment e β
 
-theorem id_α_well_typed : valid_judgment α Ty → valid_judgment ($ id, α) Σ[::[nil, id, nil],
-  ::[::[($ ::[α, Ty], id), Ty], nil]] := by
+/-
+id : Σ[::[nil, id, nil], Ty]
+assuming id α has Ty in scope,
+we should get ::[Γ, ::[::[α, Ty], Ty]]
+this is just nil.
+-/
+
+theorem id_α_well_typed : ValidJudgment α Ty → ValidJudgment ($ id, α) Σ[::[id, id], ::[::[α, Ty], Ty]] := by
   intro h_t
-  apply valid_judgment.app
-  apply valid_judgment.id
-  exact h_t
-  apply IsStep.sigma
+  apply ValidJudgment.def_eq
+  apply ValidJudgment.app
+  apply ValidJudgment.id
+  apply ValidJudgment.def_eq h_t
+  apply DefEq.symm
+  apply DefEq.trans
+  apply DefEq.step
+  apply IsStep.sapp
+  apply DefEq.step
+  apply IsStep.nil
+  apply DefEq.trans
+  apply DefEq.sigma_t
+  apply DefEq.lleft
+  apply DefEq.lright
+  apply DefEq.trans
+  apply DefEq.step
+  apply IsStep.sapp
+  apply DefEq.step
+  apply IsStep.nil
+  apply DefEq.refl
 
-example : valid_judgment α Ty → valid_judgment x α → valid_judgment ($ id, α, x) α := by
+example : ValidJudgment α Ty → ValidJudgment x α → ValidJudgment ($ id, α, x) α := by
   intro h_t h_t_x
-  apply valid_judgment.app
+  apply ValidJudgment.def_eq
+  apply ValidJudgment.app
   exact (@id_α_well_typed α h_t)
+  apply ValidJudgment.def_eq
+  exact h_t_x
   
   sorry
 

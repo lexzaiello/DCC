bb201cf6ff3ba8a79b289c202c83521cb5225ec3
Alexandra Aiello <dowlandaiello@gmail.com>
2026-02-02 04:35:24 -0800

    new id progress

--- Cm/Questions/PiListApp.lean
+++ Cm/Questions/PiListApp.lean
@@ -1,99 +1,26 @@
 import Mathlib.Data.Nat.Notation
 
 /-
-#1: Is it possible to merge Pi and lists?
+Summary of changes:
 
-  It would be ideal if Pi could handle the substitution on lists, but if we could make Pi from a list.
+- We can actually compose Pi ::[Pi, stuff], and this will induce a normal
+Pi expression eventually, but not until we way.
+- ($ Pi, ::[t_in, t_out]) - this is insert. This will just do nothing if we try
+to substitute.
+  - We shouldn't end up with ($ Pi, ::[t_in, t_out]), except after
+  running fst or snd to get input / output.
+- Removed arg from fst / snd. just snd _ _ ::[a, b]. This will be easier to work
+with and avoid redundancy.
+we can compose fst and snd.
+- Applied Pi is a type, but otherwise, it is an applicable value.
+  - Applied Pi, we don't substitute at all?
 
-  - Pi t_in t_out map_arg
-    ^
-  Note that t_in depends on map_arg
-  and t_out depends on map_arg
-
-  so we are essentially substituting.
-
-#2: Is it possible to upgrade our lists to do proper substitution?
-
-  ::[a, b] x = ::[(a x), (b x)]. NO, see #3.
-
-  This could be nice, since it propogates the value easier than we could with both.
-  Remember: list application is a special case anyway.
-
-#3: Can we make our "substitution" operation comport with our interpration of sigma pairs?
-
-  ::[a, b] - a ought to depend on b, not the other way around. Cons is most efficient in this position.
-
-  ::[(a : (β : α → Type)), (b : α)] : ::[β, α]?
-
-  So, substitution ought to proceed like such:
-
-  ::[a, b] x = ::[(a (b x)), (b x)]. This way, the type dependency is preserved.
-
-  What about nesting? Seems fine.
-
-  The head element was updated, and the tail was updated.
-
-#4: Can we still do composition with this semantics? Is it possible to make substitution "nicer"?
-or at least behave as before?
-
-  ::[g, f] x = fst ::[(g (f x)), (f x)]
-
-#5: With our Prod types, or at the very least with this type dependency, would it be easier to type fst and snd?
-
-  ::[(g : α), (f : β)]
-  fst α β : Prod α β → (α fst.snd)
-
-#6: If we form Pi from a list, then we are composing substitution in map_arg with
-  substitution in t_in t_out
-
-  Pi t_in t_out map_arg = ::[both ::[nil t_in, t_out], map_arg]
-
-  Pi ::[(both ::[t_in, t_out]), map_arg]
-
-  So Pi is basically just a marker that this is a Type?
-  Is Prod also subsumed by this? I think so, actually.
-
-  ::[(a : (β : α → Type)), (b : α)] : Pi ::[β, ($ id, α)]
-
-#7: Now that we have substitution, is point-free cons necessary? Probably not, but why?
-
-#8: Should fst and snd be in head or tail position?
-  - Our pairs might not necessarily be functions. We should prefer head position.
-  It's also easier to use, and bridges the gap.
-
-#9: What about grabbing fst after an operation. Can we do that easily?
-
-  ::[fst, my_op] x = ::[(fst (my_op x)), my_op x]
-
-  Kind of. At some point, we will need something like S for terms. What's bridging this gap for us?
-  We can embed applications with composition quite easily.
-
-  We could also make fst be cleverer by substituting into a list.
-
-Core design changes:
-- ::[a, b] x = ::[a (b x), b x] - list substitution works more as expected now.
-And, it acts like composition out of the box.
-  - Pair and Pi are just syntax markers. That's all. Pi l arg = Pi (l arg). Pair l arg = Pair (l arg). For the kernel to interpret.
-  - Can still do composition
-- Explicit fst and snd combinators are important. And, we can express their types more easily, now.
-  - fst ::[a, b] arg = a (b arg)
-  - snd ::[a, b] arg = b arg
-  - Bridge from lists to app terms.
-- Prod and Pi coincide exactly with substitution now.
-- fst and snd are in head position, not tail position.
-
-Remaining questions:
-- Can we make Pi point-free? Answer: no, it's a useful syntactic marker. Same with Prod.
-- We might benefit from a "take" combinator.
-- How will we type sigma pairs? ::[(x : (α : β → Type)), (xs : β)] : Pair ::[α, β]
-- Will we still need separate app rules for Pi vs Pair? Probably. I think it makes sense. These are very discrete things.
-
-Does both still preserve the type dependency? Seems like not really. Not sure about this one. Will find the answer though.
+($ Pi, ::[α, α])
 -/
 
 inductive Expr where
   | app    : Expr → Expr → Expr
-  | cons   : Expr → Expr → Expr
+  | cons   : Expr
   | Pi     : Expr
   | Prod   : Expr
   | ty     : Expr
@@ -111,7 +38,7 @@ syntax "($" term,+ ")"        : term
 
 macro_rules
   | `(::[ $x:term ]) => `($x)
-  | `(::[ $x:term, $xs:term,* ]) => `(Expr.cons $x ::[$xs,*])
+  | `(::[ $x:term, $xs:term,* ]) => `(Expr.app (Expr.app Expr.cons $x) ::[$xs,*])
   | `(($ $x:term) ) => `($x)
   | `(($ $f:term, $x:term )) => `(Expr.app $f $x)
   | `(($ $f, $x:term, $args:term,* )) => `(($ (Expr.app $f $x), $args,*))
@@ -121,17 +48,15 @@ notation "Ty" => Expr.ty
 open Expr
 
 inductive IsStep : Expr → Expr → Prop
-  | sapp   : IsStep ($ ::[a, b], x) ::[($ a, ($ b, x)), ($ b, x)]
-  | fst    : IsStep ($ fst, α, β, γ, ::[a, b], arg) ($ a, ($ b, arg))
-  | snd    : IsStep ($ snd, α, β, γ, ::[a, b], arg) ($ b, arg)
+  | sapp   : IsStep ($ ::[a, b], x)  ::[a, ($ b, x)]
+  | fst    : IsStep ($ fst, α, β, ::[a, b]) ($ a, b)
+  | snd    : IsStep ($ snd, α, β, ::[a, b]) b
   | nil    : IsStep ($ nil, α, x) α
   | id     : IsStep ($ Expr.id, _α, x) x
-  -- both α β γ (f : γ) (g : β) (arg : α) = ::[($ const', ($ γ, arg), ($ β, arg), ($ f, arg)), g] arg
-  | both   : IsStep ($ both, _α, β, γ, f, g, x) ::[($ const', ($ γ, arg), ($ β, arg), ($ f, arg)), ($ g, arg)]
+  | both   : IsStep ($ both, _α, _β, _γ, f, g, arg)
+    ::[($f, arg), ($ g, arg)]
   | const' : IsStep ($ const', _α, _β, x, y) x
   | const  : IsStep ($ const, _α, _β, x, y) x
-  | pi     : IsStep ($ ($ Pi, l), x) ($ l, x)
-  | prod   : IsStep ($ ($ Prod, l), x) ($ l, x)
   | left   : IsStep f f'
     → IsStep ($ f, x) ($ f', x)
   | right  : IsStep x x'
@@ -150,109 +75,64 @@ inductive DefEq : Expr → Expr → Prop
   | right   : DefEq x x'  → DefEq ($ f, x) ($ f, x')
   | lright  : DefEq f f'  → DefEq ::[x, f] ::[x, f']
   | lleft   : DefEq x x'  → DefEq ::[x, f] ::[x', f]
-  | subst   : DefEq ($ ($ Pi, bdy), x) ($ ($Pi, bdy₂), x)
+  | subst   : DefEq ($ fst, ($ bdy, x)) ($ fst, ($ bdy₂, x))
+    → DefEq ($ snd, ($ bdy, x)) ($ snd, ($ bdy₂, x))
     → DefEq ($ Pi, bdy) ($ Pi, bdy₂)
 
-/-
-Cons is typed, by default, as Pair ::[α, β]
-Pi expects any list, but it expects specifically t_in and t_out registers, at least.
-
-Another thought on both:
-both α β γ (f : γ) (g : β) (arg : α) = ::[($ const', ($ γ, arg), ($ β, arg), ($ f, arg)), g] arg
-γ : α → Type
-β : α → Type
-γ arg
-β arg
-
-Does this both rule work as expected?
-Maybe not?
-
-Can we stil derive Pi?
-
-It feels like by default we should assume that there is dependency from
-map_arg -> t_in -> t_out.
-
-And if we don't want it, we use both.
-
-This way, Pi is not complex at all.
-
-Pi : ∀ {map_arg : α → β} {t_in : β → γ} {t_out : γ → δ}, Prod ::[t_out, ::[t_in, map_arg]] → Type
-
-since prod members are preserved, we can assume
-
-- fst ::[a, b] arg = a (b arg)
-- snd ::[a, b] arg = b arg
-
-t_out:   Pi l, fst l
-t_in:    Pi l, fst (snd l)
-map_arg: NOT NECESSARY NOW. we get dependency by default.
-
-So, Pi : Prod ::[t_out, t_in] → Type
-
-Prod : ∀ {α : Type} {β : α → Type} {γ : Type → Type}, Prod ::[γ, β] → Type
-
-So, how do we make this arrow?
-
-Prod ::[t_out, t_in] → Type = Prod ::[($ nil, Type), Prod ::[t_out, t_in]]
-
-what about fst type?
-
-fst α β γ : (p : Prod ::[γ, β]) → (x : α) → (γ (snd α β γ p arg))
-snd α β γ : (p : Prod ::[γ, β]) → (x : α) → (β arg)
-
-fst ::[a, b] arg = a (b arg)
-
-Feels like fst and snd might need to be special cased, but not sure.
-
-So what's id's type then?
-
-id : ∀ (α : Type), α → α
-
-inner pi receives α
-Pi (both Ty _ _ (Pi ::[nil, nil]
-
-Question: how do we handle contexts? with both, almost certainly.
-
-What if we do it in reverse?
-second argument gets substituted first,
-then first gets its value, and it feels like
-the output will mention a more complex term than the input.
-
-id α x, x is a term. T_OUT GOES SECOND.
-
-than the first.
-
-id α : Pi ::[t_in, t_out]
-
-| sapp   : IsStep ($ ::[a, b], x) ::[($ a, ($ b, x)), ($ b, x)]
-
-id α : ::[Ty, ::[α_in, nil]]
-
-α_in receives ($ nil, (α : Ty)) and then (α : Ty)
-($ nil, (α : Ty)) : 
-so α_in = ($ const', ?, Ty,
-
-id α x : ::[($ nil, α, x), ($ nil, α, x)]
--/
-
-/-
-nil (α : Type) 
--/
-def nil.type : Expr :=
-  
-
 def id.type : Expr :=
-  sorry
+  ($ Pi, ::[($ nil, Ty), ::[Pi, ::[($ id, Ty), nil]]])
 
 inductive ValidJudgment : Expr → Expr → Prop
-  | cons : ValidJudgment xs β
-    → ValidJudgment x ($ Pi, ::[α, β])
-    → ValidJudgment ::[x, xs] ($ Prod, ::[($ Pi, ::[α, β]), β])
-  | Pi   : ValidJudgment Pi   ($ Pi, ::[($ nil, Ty), Prod])
-  | Prod : ValidJudgment Prod ($ Pi, ::[($ nil, Ty), Prod])
-  | app  : ValidJudgment fn ($ Pi, t)
-    → ValidJudgment t_in ($ Pi, ::[($ nil, Ty), β])
-    → ValidJudgment t_out ($ Pi, ::[β, α])
-    → ValidJudgment arg ($ fst, α, t_out, t_in, ($ t, arg))
-    → ValidJudgment ($ fn, arg) ($ snd, α, t_out, t_in, ($ t, arg))
+  | cons  : ValidJudgment xs β
+    → ValidJudgment x α
+    → ValidJudgment ::[x, xs] ($ Pi, ::[α, β])
+  | Pi    : ValidJudgment Pi   ($ Pi, ::[($ nil, Ty), Prod])
+  | Prod  : ValidJudgment Prod ($ Pi, ::[($ nil, Ty), Prod])
+  | app   : ValidJudgment fn ($ Pi, t)
+    → ValidJudgment arg ($ fst, Ty, Ty, ($ t, arg))
+    → ValidJudgment ($ fn, arg) ($ snd, Ty, Ty, ($ t, arg))
+  | id    : ValidJudgment id id.type
+  | ty    : ValidJudgment Ty Ty
+  | defeq : ValidJudgment e t₁
+    → DefEq t₁ t₂
+    → ValidJudgment e t₂
+
+syntax "defeq" ident,*        : tactic
+syntax "step" ident,*         : tactic
+syntax "judge" ident,*         : tactic
+
+macro_rules
+  | `(tactic| defeq $fn:ident,*) => do
+    let nms : Array (Lean.TSyntax `tactic) ← (Array.mk <$> (fn.getElems.toList.mapM (fun name =>
+      let nm := Lean.mkIdent (Lean.Name.mkStr `DefEq name.getId.toString)
+      `(tactic| apply $nm))))
+
+    `(tactic| $[$nms];*)
+  | `(tactic| step $fn:ident,*) => do
+    let nms : Array (Lean.TSyntax `tactic) ← (Array.mk <$> (fn.getElems.toList.mapM (fun name =>
+      let nm := Lean.mkIdent (Lean.Name.mkStr `IsStep name.getId.toString)
+      `(tactic| apply $nm))))
+
+    `(tactic| $[$nms];*)
+  | `(tactic| judge $fn:ident,*) => do
+    let nms : Array (Lean.TSyntax `tactic) ← (Array.mk <$> (fn.getElems.toList.mapM (fun name =>
+      let nm := Lean.mkIdent (Lean.Name.mkStr `ValidJudgment name.getId.toString)
+      `(tactic| apply $nm))))
+
+    `(tactic| $[$nms];*)
+
+example : ValidJudgment α Ty → ValidJudgment x α → ValidJudgment ($ id, α, x) α := by
+  intro h_t_α h_t_x
+  judge defeq, app, defeq, app, id, defeq
+  assumption
+  defeq symm, trans, right, trans, step
+  step sapp
+  defeq right, step
+  step sapp
+  defeq trans, step
+  step fst
+  defeq trans, step
+  step nil
+  defeq refl
   
+  sorry

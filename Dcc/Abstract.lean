981e7be3eab5fee540050dfab3d73df01a3455ae
Alexandra Aiello <dowlandaiello@gmail.com>
2026-02-14 15:07:41 -0800

    proof that curry's Xi implies our elim rules

---
+++ Dcc/Abstract.lean
@@ -0,0 +1,69 @@
+/-
+Representations of our core inference rules in Lean, with some proofs about them.
+-/
+
+/-
+Cons / sigma / composition:
+(:: f g) x = (:: f(x), g(f(x)))
+
+This is a core data structure in the paper.
+fst / snd correspond to left and right sigma elimination.
+
+fst (:: f g) x = fst (:: f(x), g(f(x))) = f(x)
+snd (:: f g) x = snd (:: f(x), g(f(x))) = g(x)
+
+Curry has a concept of Ξ / restricted generality
+in illative combinatory logic with the inference rule:
+
+Δ ⊢ ΞXY      Δ ⊢ XU
+-------------------
+       Δ ⊢ YU
+
+Note that this specific rule is designed to prevent Curry's paradox.
+-/
+def Ξ.elim {α : Type} {P : α → Prop} {Q : α → Prop}
+  (Ξ : ∀ (x : α), P x → Q x)
+  (X : ∀ (x : α), P x) (U : α) : Q U := by
+  exact Ξ _ (X U)
+
+/-
+P1) ΞXY requires that P(U) implies Q(U)
+P2) XU is a proof of P(U)
+The conclusion YU is a proof of Q(U) derived from P1, P2
+-/
+
+/-
+In our calculus, we have a somewhat similar notion of sigma composition
+and left / right elimination / projection.
+
+(:: f g) x = (:: f(x), g(f(x)))
+fst (:: f g) x = fst (:: f(x), g(f(x))) = f(x)
+snd (:: f g) x = snd (:: f(x), g(f(x))) = g(x)
+
+Here are the elimination rules written out:
+-/
+
+/-
+(:: f g) x = (:: f(x), g(f(x)))
+-/
+def Cons.elim_right {α : Type} {P : α → Prop} {Q : ∀ {x : α}, P x → Prop}
+  (f : ∀ (x : α), P x) (g : ∀ {x : α}, Q (f x)) (x : α) : Q (f x) := by
+  apply g
+
+def Cons.elim_left {α : Type} {P : α → Prop} {Q : ∀ {x : α}, P x → Prop}
+  (f : ∀ (x : α), P x) (_g : ∀ {x : α}, Q (f x)) (x : α) : P x := by
+  apply f
+
+/-
+A proof that Ξ implies elim_right and elim_left.
+-/
+theorem Xi_imp_cons_elim {α : Type} {P : α → Prop} {Q₁ : α → Prop}
+  (Ξ : ∀ (x : α), P x → Q₁ x)
+  (X : ∀ (x : α), P x) : ∃ (Q : ∀ {x : α}, P x → Prop) (_g : ∀ {x : α}, Q (X x)),
+  ∀ (x : α), Q (X x) ∧ P x := ⟨fun {x} _hp => Q₁ x
+    , fun {x} => Ξ _ (X x)
+    , fun x => ⟨Ξ _ (X x), X _⟩⟩
+
+/-
+A proof that Curry's paradox is not possible with our inference rule:
+-/
